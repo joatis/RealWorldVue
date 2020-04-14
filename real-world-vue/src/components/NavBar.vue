cbea1043cff9b3e550c7b4587e0296d4de78c81f
John Oatis <joatis@joatis3.com>
2020-04-14 15:19:17 -0400

    Single File Vue Components
    Added scss support, nested components. Used global and scoped styles.

---
+++ real-world-vue/src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="nav">
+    <router-link to="/" class="brand">Real World Events</router-link>|
+
+    <nav>
+      <router-link :to="{ name: 'event-list' }">List</router-link>|
+      <router-link :to="{ name: 'event-create' }">Create</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
