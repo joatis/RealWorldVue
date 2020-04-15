79f2f0bf13ab156c344e32f3868cafbdc53e086f
John Oatis <joatis@joatis3.com>
2020-04-15 10:55:48 -0400

    Mutated state, Added actions with mutations, added events  to state

--- real-world-vue/src/store/index.js
+++ real-world-vue/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService'
 
 Vue.use(Vuex)
 
@@ -23,8 +24,18 @@ export default new Vuex.Store({
       { id: 2, title: '...' }
     ]
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    }
+  },
   modules: {},
   getters: {
     catLength: state => {
