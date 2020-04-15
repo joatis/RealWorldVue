11914d8c298070c732776435837887fd01cfa804
John Oatis <joatis@joatis3.com>
2020-04-15 09:56:55 -0400

    Mastering Vuex Added and access state. Getters for derived state, mapping state and getters to computed properties

--- real-world-vue/src/store/index.js
+++ real-world-vue/src/store/index.js
@@ -1,11 +1,37 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {
+      id: 'abc123',
+      name: 'John Oatis'
+    },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: [
+      { id: 1, title: '...' },
+      { id: 2, title: '...' }
+    ]
+  },
   mutations: {},
   actions: {},
-  modules: {}
-});
+  modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
+})
--- real-world-vue/src/views/EventCreate.vue
+++ real-world-vue/src/views/EventCreate.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>Create Events</h1>
+  <div>
+    <h1>Create and Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.name }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <p>{{ getEventById(1) }}</p>
+    <ul>
+      <li v-for="category in categories" :key="category.id">{{ category }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapGetters(['getEventById']),
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
